994c224597de7348f836ff98d2ed96483b391ba4
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-21 01:17:07 +0300

    Fixed footer width

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -21,44 +21,45 @@ export default {
 </script>
 
 <template>
-  <footer
-    class="flex flex-col items-center justify-between gap-10 bg-very-dark-violet py-14 lg:flex-row lg:items-start lg:px-20 xl:px-40"
-  >
-    <ShortlyLogo class="fill-white" />
+  <footer class="gap-10 bg-very-dark-violet py-14 px-6">
+    <nav
+      class="mx-auto flex max-w-6xl flex-col items-center justify-between lg:flex-row lg:items-start"
+    >
+      <ShortlyLogo class="fill-white" />
+      <div class="flex flex-col gap-8 lg:flex-row xl:gap-20">
+        <FooterLinkList>
+          <template #heading>Features</template>
 
-    <div class="flex flex-col gap-8 lg:flex-row xl:gap-20">
-      <FooterLinkList>
-        <template #heading>Features</template>
+          <FooterLink>Link Shortening</FooterLink>
+          <!-- <a href="#">Link Shortening</a> -->
+          <FooterLink>Branded Links</FooterLink>
+          <FooterLink>Analytics</FooterLink>
+        </FooterLinkList>
 
-        <FooterLink>Link Shortening</FooterLink>
-        <!-- <a href="#">Link Shortening</a> -->
-        <FooterLink>Branded Links</FooterLink>
-        <FooterLink>Analytics</FooterLink>
-      </FooterLinkList>
+        <FooterLinkList>
+          <template #heading>Resources</template>
 
-      <FooterLinkList>
-        <template #heading>Resources</template>
+          <FooterLink>Blog</FooterLink>
+          <FooterLink>Developers</FooterLink>
+          <FooterLink>Support</FooterLink>
+        </FooterLinkList>
 
-        <FooterLink>Blog</FooterLink>
-        <FooterLink>Developers</FooterLink>
-        <FooterLink>Support</FooterLink>
-      </FooterLinkList>
+        <FooterLinkList>
+          <template #heading>Company</template>
 
-      <FooterLinkList>
-        <template #heading>Company</template>
+          <FooterLink>About</FooterLink>
+          <FooterLink>Our Team</FooterLink>
+          <FooterLink>Careers</FooterLink>
+          <FooterLink>Contact</FooterLink>
+        </FooterLinkList>
 
-        <FooterLink>About</FooterLink>
-        <FooterLink>Our Team</FooterLink>
-        <FooterLink>Careers</FooterLink>
-        <FooterLink>Contact</FooterLink>
-      </FooterLinkList>
-
-      <div class="flex gap-6">
-        <FooterLink><IconFacebook /></FooterLink>
-        <FooterLink><IconTwitter /></FooterLink>
-        <FooterLink><IconPinterest /></FooterLink>
-        <FooterLink><IconInstagram /></FooterLink>
+        <div class="flex gap-6">
+          <FooterLink><IconFacebook /></FooterLink>
+          <FooterLink><IconTwitter /></FooterLink>
+          <FooterLink><IconPinterest /></FooterLink>
+          <FooterLink><IconInstagram /></FooterLink>
+        </div>
       </div>
-    </div>
+    </nav>
   </footer>
 </template>
