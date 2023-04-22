61d2d7db1c233f55e2a02ce636d07e5795ff599a
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-23 00:31:32 +0300

    Header padding corrections

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-  <header class="px-6 pb-7 pt-10 md:pb-12 md:pt-14">
+  <header class="px-6 pb-7 pt-10 md:pb-20 md:pt-12">
     <nav class="mx-auto flex w-full max-w-6xl items-center justify-between">
       <div class="flex gap-12">
         <a href="#"><ShortlyLogo class="fill-very-dark-violet" /></a>
