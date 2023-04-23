1f7d10fec57b4def9a2b8fe39efd8525617d39c5
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-23 05:05:09 +0300

    Edit vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-mentor-url-shortening-api/',
   plugins: [vue()],
   resolve: {
     alias: {
