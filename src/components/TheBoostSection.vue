b53169f5445951ed305bf26111b29fde1fa9d8ae
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-24 03:16:21 +0300

    Fix boost section text center on very small screens

--- src/components/TheBoostSection.vue
+++ src/components/TheBoostSection.vue
@@ -12,7 +12,7 @@ export default {
   <div
     class="flex h-[300px] flex-col items-center justify-center gap-6 bg-dark-violet bg-boost-mobile bg-right-top bg-no-repeat md:h-[250px] md:bg-boost-desktop"
   >
-    <p class="text-3xl font-bold text-white md:text-4xl">Boost your links today</p>
+    <p class="text-center text-3xl font-bold text-white md:text-4xl">Boost your links today</p>
 
     <AppButton>Get Started</AppButton>
   </div>
